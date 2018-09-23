7d6aaf2893bc25ccd09372bf24e3687059502216
ronron <ant@n.io>
2018-09-23 17:56:50 +0200

    .

--- manifests/config.pp
+++ manifests/config.pp
@@ -14,4 +14,34 @@ class postfix::config (
       notify   => Service[$service_name],  
     }
 
+    file { $postfix_db_files :
+			ensure => present
+      notify   => Exec[$postmap_file],  
+
+		exec { '/usr/sbin/postmap $postfix_db_file':
+			alias   => 'postmap_file'
+			cwd     => '/var/tmp',
+			path    => ['/usr/bin', '/usr/sbin',],
+		}
+
+    #file { "${configuration_directory}/$db_files" :
+    #  ensure => present,
+    #  notify   => Exec[postmap_file],
+    #}  
+
+    #exec { "/usr/sbin/postmap ${configuration_directory}/${db_file}":
+    #  alias   => 'postmap_file',
+    #  cwd     => '/var/tmp',
+    #  path    => ['/usr/bin', '/usr/sbin',],        
+    #  refreshonly => true,
+    #}
+
+    exec { 'my_command':
+    command   => "/usr/sbin/postmap ${configuration_directory}/${db_file}",
+    subscribe => [
+    File["${configuration_directory}/${db_file}"],
+  ],
+  refreshonly => true,
+  }
+
 }
