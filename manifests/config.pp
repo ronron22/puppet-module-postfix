4cfb5f519cb09db3d8b68f5e03a054938ec9b831
ronron <ant@n.io>
2018-09-28 11:33:37 +0200

    .

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,35 +7,67 @@ class postfix::config (
     
 	) inherits postfix {
 
-    file { $configuration_directory :
-      ensure => present,
-      sourceselect => all,
-      source => ["puppet:///modules/postfix/${nodename}/etc/postfix","/etc/postfix "],
-      recurse => true,
-      notify   => Service[$service_name],  
+
+  $config_files.each |String $file| {
+    file { "${configuration_directory}${file}":
+      content => file("postfix/${nodename}/${configuration_directory}${file}"),
+      require => File["${configuration_directory}"],
     }
+  }
 
-		$db_files.each |String $dbfile| {
-			file { "/etc/postfix/${dbfile}":
-				audit => 'content',
-			}
+  $access_files.each |String $file| {
+    file { "${configuration_directory}${file}":
+      content => file("postfix/${nodename}/${configuration_directory}${file}"),
+      require => File["${configuration_directory}"],
     }
+  }
 
-    $db_files.each |String $dbfile| {
-			exec {"postfix.recreate.${dbfile}":
-				command => "/usr/sbin/postmap ${configuration_directory}/${dbfile}",
-				path        => ['/usr/bin', '/usr/sbin'],
-				subscribe   => File["${configuration_directory}/${dbfile}"],
-				refreshonly => true,
-			}
-		}
+  file { "${configuration_directory}${postfix_files_directory}":
+    ensure => 'directory',
+    mode    =>  '0755',
+  }
 
-		$db_files.each |String $dbfile| {
-			exec {"postfix.create.${dbfile}":
-				command => "/usr/sbin/postmap ${configuration_directory}/${dbfile}", 
-				path        => ['/usr/bin', '/usr/sbin'],
-				onlyif =>  "test ! -f ${configuration_directory}/${dbfile}.db",
-			}
-		}
+  $postfix_files_content.each |String $file| {
+    file { "${configuration_directory}${postfix_files_directory}}${file}":
+      content => file("postfix/${nodename}${configuration_directory}${postfix_files_directory}}${file}"),
+      require => File["${configuration_directory}${postfix_files_directory}"],
+    }
+  }
+
+  file { "${configuration_directory}${sasl_files_directory}":
+    ensure => 'directory',
+    mode    =>  '0755',
+  }
+
+  $sasl_files.each |String $file| {
+    file { "${configuration_directory}${sasl_files_directory}${file}":
+      content => file("postfix/${nodename}${configuration_directory}${sasl_files_directory}${file}"),
+      require => File["${configuration_directory}${sasl_files_directory}"],
+    }
+  }
+
+  $db_files.each |String $dbfile| {
+    file { "${configuration_directory}${dbfile}":
+      audit => 'content',
+      require => File["${dbfile}"],
+    }
+  }
+
+  $db_files.each |String $dbfile| {
+    exec {"postfix.recreate.${dbfile}":
+      command => "/usr/sbin/postmap ${configuration_directory}/${dbfile}",
+      path        => ['/usr/bin', '/usr/sbin'],
+      subscribe   => File["${configuration_directory}${dbfile}"],
+      refreshonly => true,
+    }
+  }
+
+  $db_files.each |String $dbfile| {
+    exec {"postfix.create.${dbfile}":
+      command => "/usr/sbin/postmap ${configuration_directory}/${dbfile}", 
+      path        => ['/usr/bin', '/usr/sbin'],
+      onlyif =>  "test ! -f ${configuration_directory}/${dbfile}.db",
+    }
+  }
 
 }
