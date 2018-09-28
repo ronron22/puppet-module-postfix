ded5c67d0c9686e9913a224cc4dd8e02c0a0bd4e
ronron <ant@n.io>
2018-09-28 13:31:19 +0200

    .

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,48 +1,48 @@
 class postfix::config (
 
-	$nodename = hiera('nodename'),
-	$configuration_directory = hiera('configuration_directory'),
-	$service_name = hiera('postfix::service::service_name'),
-	$db_files = hiera('db_files'),
+	String $configuration_directory,
+	String $service_name = hiera('postfix::service::service_name'),
+	Array $db_files,
+  String $sasl_files_directory = "${configuration_directory}${sasl_files_directory} 
+  String $postfix_files_directory = "${configuration_directory}${postfix_files_directory} 
     
 	) inherits postfix {
 
-
   $config_files.each |String $file| {
     file { "${configuration_directory}${file}":
-      content => file("postfix/${nodename}/${configuration_directory}${file}"),
+      content => file("postfix/${::fqdn}/${configuration_directory}${file}"),
       require => File["${configuration_directory}"],
     }
   }
 
   $access_files.each |String $file| {
     file { "${configuration_directory}${file}":
-      content => file("postfix/${nodename}/${configuration_directory}${file}"),
+      content => file("postfix/${::fqdn}/${configuration_directory}${file}"),
       require => File["${configuration_directory}"],
     }
   }
 
-  file { "${configuration_directory}${postfix_files_directory}":
+  file { "${postfix_files_directory}":
     ensure => 'directory',
     mode    =>  '0755',
   }
 
   $postfix_files_content.each |String $file| {
-    file { "${configuration_directory}${postfix_files_directory}}${file}":
-      content => file("postfix/${nodename}${configuration_directory}${postfix_files_directory}}${file}"),
-      require => File["${configuration_directory}${postfix_files_directory}"],
+    file { "${postfix_files_directory}${file}":
+      content => file("postfix/${::fqdn}$${postfix_files_directory}}${file}"),
+      require => File["${postfix_files_directory}"],
     }
   }
 
-  file { "${configuration_directory}${sasl_files_directory}":
+  file { "${sasl_files_directory}":
     ensure => 'directory',
     mode    =>  '0755',
   }
 
   $sasl_files.each |String $file| {
-    file { "${configuration_directory}${sasl_files_directory}${file}":
-      content => file("postfix/${nodename}${configuration_directory}${sasl_files_directory}${file}"),
-      require => File["${configuration_directory}${sasl_files_directory}"],
+    file { "${sasl_files_directory}${file}":
+      content => file("postfix/${::fqdn}${sasl_files_directory}${file}"),
+      require => File["${sasl_files_directory}"],
     }
   }
 
