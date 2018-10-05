b29195234a53630ab7e4fd4ac7de1b0a6c2f3c17
ronron <ant@n.io>
2018-10-05 21:54:36 +0200

    .

--- manifests/init.pp
+++ manifests/init.pp
@@ -119,10 +119,10 @@ class postfix (
   Optional[String]  $virtual_gid_maps = undef,
   Optional[String]  $virtual_transport = undef,
 
-  Optional[Boolean] $enable_cyrus = undef,
-  Optional[Boolean] $enable_policyd_spf = undef,
-  Optional[Boolean] $enable_submission = undef,
-  Optional[Boolean] $enable_smtps = undef,
+  Optional[Boolean] $cyrus = undef,
+  Optional[Boolean] $policyd_spf = undef,
+  Optional[Boolean] $submission = undef,
+  Optional[Boolean] $smtps = undef,
 
   ) {
 
